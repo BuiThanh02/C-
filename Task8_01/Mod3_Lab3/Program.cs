a67afb54ac2f404ea5bc2ec93c1491e8c8c32c00
BuiThanh02 <73330613+BuiThanh02@users.noreply.github.com>
2021-07-13 21:09:19 +0700

    .

---
+++ Task8_01/Mod3_Lab3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Mod3_Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetStudentInformation();
+        }
+
+        static void GetStudentInformation()
+        {
+            Console.WriteLine("Enter the student's first name: ");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Enter the student's last name: ");
+            string lastname = Console.ReadLine();
+            Console.WriteLine("Enter the student's birthday: ");
+            string birthday = Console.ReadLine();
+            PrintStudentDetails(firstName, lastname, birthday);
+        }
+
+        static void PrintStudentDetails(string first, string last, string birthday)
+        {
+            Console.WriteLine("{0} {1} was born on: {2}", first, last, birthday);
+        }
+    }
+}
